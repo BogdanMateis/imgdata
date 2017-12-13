e2759327c8ee28f83aeb69e50e123ac3aae6987d
BogdanMateis <bogdan.mateis@yahoo.com>
2017-12-13 14:31:20 +0200

    added more data

--- functiiR.R
+++ functiiR.R
@@ -32,3 +32,25 @@ fileName <- "D:/date.txt" #calea catre fisierul cu datele
 makemodel(fileName, 2)
 
 
+makemodelArray<- function(dataset,repeats)
+{
+  accuracies<-vector(mode="numeric",length=repeats);
+  for(i in 1:repeats){
+    data <- datas
+    noOfSamples <- nrow(data);
+    random <- sample (1:noOfSamples,replace=FALSE);
+    noOfTrainingSamples <- round(0.66* noOfSamples);
+    randomTrain <- random[1:noOfTrainingSamples];
+    randomTest <- random[(noOfTrainingSamples +1):noOfSamples];
+    train <- data[randomTrain,];
+    test <- data[randomTest,];
+    claseTest<- train[,c("tip")];
+    train <- subset(train, select=- tip);
+    svm_model <- svm(train,claseTest);
+    clase <- test[,c("tip")];
+    test <- subset(test, select=- tip);
+    pred <- predict(svm_model,test);
+    accuracies[i] <- classAgreement(table(pred,clase))$diag;
+  }
+  return(mean(accuracies));
+}
---
+++ machine learning/treeBreastproblem.R
@@ -0,0 +1,53 @@
+library("mlbench")
+library(rpart)
+library(datasets)
+library("e1071")
+library(ROCR) # pentru ROC
+library(stats) 
+library(fmsb)
+data(BreastCancer)
+dat<-na.omit(BreastCancer)
+dat<-dat[,-1]
+classColumn<-10
+repeats<-30;
+meanaccuracy<-function(dat,repeats){
+index<-1:nrow(dat)
+accuraciesTree<-vector(mode="numeric",length=repeats)
+accuraciesSVM<-vector(mode="numeric",length=repeats)
+for(i in 1:repeats){
+  #random subsampling 1/3 data
+testindex<-sample(index,trunc(length(index)/3))
+testset<-dat[testindex,]
+trainset<-dat[-testindex,]
+
+#learning
+svm.model<-svm(Class~., data = trainset, kernel = "linear", cost = 1, probability = TRUE)
+rpart.model<- rpart(Class~.,trainset,method="class")
+#predicting
+predTree<- predict(rpart.model,testset[,-classColumn],type="class")
+predSVM<- predict(svm.model,testset[,-classColumn],type="class")
+
+#curba ROC
+probSVM<-predict(svm.model,testset[,-classColumn],probability=TRUE)
+svm.predictii<-attr(probSVM,"probabilities")[,1]
+reale<-testset[classColumn][,1]
+etichete<-as.integer(reale)-1
+pred<-prediction(as.vector(probSVM),etichete)
+
+#matricea de confuzie
+confusionTree<-table(predTree,testset[,classColumn])
+confusionSVM<-table(predSVM,testset[,classColumn])
+
+accuraciesTree[i]<-classAgreement(confusionTree)$diag
+accuraciesSVM[i]<-classAgreement(confusionSVM)$diag
+}
+#acuratetea medie, deviatia standard
+print(mean(accuraciesTree))
+print(mean(accuraciesSVM))
+print(sqrt(var(accuraciesTree)))
+print(sqrt(var(accuraciesSVM)))
+Kappa.test(predTree,testset[,classColumn])
+Kappa.test(predSVM,testset[,classColumn])
+#wilcox
+wilcox.test(accuraciesSVM,accuraciesTree,paired=TRUE)
+}
