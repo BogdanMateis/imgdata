5f4c72cd785f22092bbdeb75fe11a24e3ac16157
BogdanMateis <bogdan.mateis@yahoo.com>
2018-01-10 20:16:04 +0200

    some data

--- functiiR.R
+++ functiiR.R
@@ -46,7 +46,7 @@ makemodelArray<- function(dataset,repeats)
     test <- data[randomTest,];
    # claseTest<- train[,c("tip")];
    # train <- subset(train, select=- tip);
-    svm_model <- svm(tip~.,data=train,kernel="radial",cost=9,gamma=0.1);
+    svm_model <- svm(tip~.,data=train,kernel="radial",cost=10,gamma=0.1);
     clase <- test[,c("tip")];
     test <- subset(test, select=- tip);
     pred <- predict(svm_model,test);
@@ -59,11 +59,11 @@ obj <- tune(svm, tip~., data = file7,
             tunecontrol = tune.control(sampling = "fix")
 )
 
-file1<- read.table("d:/programming/r/data/thresh211scan1ms.txt",header=TRUE)
+file1<- read.table("d:/programming/r/data/thresh213scan1.txt",header=TRUE)
 file2<- read.table("d:/programming/r/data/thresh215scan1.txt",header=TRUE)
-file3<- read.table("d:/programming/r/data/thresh160scan0ms.txt",header=TRUE)
-file4<- read.table("d:/programming/r/data/thresh160scan0.txt",header=TRUE)
-file5<- read.table("d:/programming/r/data/thresh220scan1.txt",header=TRUE)
-file6<- read.table("d:/programming/r/data/thresh221scan1ms.txt",header=TRUE)
-file7<-  read.table("d:/programming/r/data/thresh215scan1.txt",header=TRUE)
-file8<-  read.table("d:/programming/r/data/thresh217scan1.txt",header=TRUE)
+file3<- read.table("d:/programming/r/data/thresh217scan1.txt",header=TRUE)
+file4<- read.table("d:/programming/r/data/thresh220scan1.txt",header=TRUE)
+file5<- read.table("d:/programming/r/data/thresh222scan1.txt",header=TRUE)
+file6<- read.table("d:/programming/r/data/thresh223scan1.txt",header=TRUE)
+file7<-  read.table("d:/programming/r/data/thresh225scan1.txt",header=TRUE)
+file8<-  read.table("d:/programming/r/data/thresh230scan1.txt",header=TRUE)
